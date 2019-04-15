9fedf4c84dc1bdde0121ff4d70f5ddbc5ffaa199
BuceaGeorgia <46248035+BuceaGeorgia@users.noreply.github.com>
2019-04-15 20:53:08 +0300

    Update Carte.h

--- Carte.h
+++ Carte.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <string>
 #include <iostream>
+
+//clasa cu obiecte de tip pet, folosita doar pentru teste
 class Pet
 {
 public:
+	
 	Pet() = default;
 	Pet(std::string t, std::string s, double p) :type{ t }, species{ s }, price{ p } {
 	}
+	//constructor de copiere
 	Pet(const Pet& ot) :type{ ot.type }, species{ ot.species }, price{ ot.price } {
 		std::cout << "copy\n";
 	}
@@ -17,6 +21,7 @@ public:
 		return type;
 	}
 private:
+	
 	std::string type;
 	std::string species;
 	double price = -1.0;
